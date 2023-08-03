0a44f342d3ad09963553ab6363b2ee9d4e2232a1
Alan Ren <10086000+alanvww@users.noreply.github.com>
2023-08-03 02:42:18 -0400

    postcss fix?

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,19 @@
 // If you want to use other PostCSS plugins, see the following:
 // https://tailwindcss.com/docs/using-with-preprocessors
 module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/**/**/*.{js,ts,jsx,tsx,mdx}',
+
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
   },
 }
