1a37c4089a52237a2d485b02e3e4d1a999b75b57
Alan Ren <10086000+alanvww@users.noreply.github.com>
2023-08-03 02:45:14 -0400

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,7 @@
 // https://tailwindcss.com/docs/using-with-preprocessors
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: { config: './tailwindcss-config.js' },
     autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
   },
 }
