54329107f2ab23bb1568b8daf431a04006a5d1d6
Alan Ren <10086000+alanvww@users.noreply.github.com>
2023-08-03 02:08:19 -0400

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,5 @@
 // If you want to use other PostCSS plugins, see the following:
 // https://tailwindcss.com/docs/using-with-preprocessors
 module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('@tailwindcss/deprecation-warnings'),
-    require('autoprefixer'),
-  ],
+  plugins: [require('tailwindcss'), require('autoprefixer')],
 }
