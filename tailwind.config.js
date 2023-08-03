316ce8c462e7c754dde05d443a854ca992aa8519
Alan Ren <10086000+alanvww@users.noreply.github.com>
2023-08-03 02:43:53 -0400

    postcss fix again and again

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/**/**/*.{js,ts,jsx,tsx,mdx}',
 
     // Or if using `src` directory:
     './src/**/*.{js,ts,jsx,tsx,mdx}',
