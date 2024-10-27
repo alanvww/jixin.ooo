50822d909a491bcef02dba008a62f5f3146e2544
Alan Ren <10086000+alanvww@users.noreply.github.com>
2024-10-26 20:56:05 -0400

    Upgrade to next.js 15 & add maintenance mode

--- components/global/HomeNavbar.tsx
+++ components/global/HomeNavbar.tsx
@@ -7,26 +7,48 @@ interface NavbarProps {
 }
 
 export function HomeNavbar({ menuItems }: NavbarProps) {
+
+
   return (
-    <div className="fixed z-10 	flex h-screen w-full flex-col place-content-center items-center gap-y-5 px-4  py-4 align-middle md:inset-y-1/2 md:h-auto md:flex-row md:gap-x-5  md:px-16 md:py-5 lg:px-32">
-      {menuItems &&
-        menuItems.map((menuItem, key) => {
-          const href = resolveHref(menuItem?._type, menuItem?.slug)
-          if (!href) {
-            return null
-          }
-          return (
-            <Link
-              key={key}
-              className={`w-full text-center  text-xl text-[#6E6E6E] hover:text-black dark:text-white dark:hover:text-[#6E6E6E]   md:w-1/3 md:text-3xl ${
-                menuItem?._type === 'home' ? 'font-extrabold ' : ''
-              }`}
-              href={href}
-            >
-              {menuItem.title}
-            </Link>
-          )
-        })}
-    </div>
+    <>
+      {process.env.NEXT_PUBLIC_MAINTENANCE_MODE === 'true' ? (
+        <div className="fixed z-10 	flex h-screen w-full flex-col place-content-center items-center gap-y-5 px-4  py-4 align-middle md:inset-y-1/2 md:h-auto md:flex-row md:gap-x-5  md:px-16 md:py-5 lg:px-32">
+          <Link
+            className={`w-full text-center  text-2xl font-extrabold text-[#6E6E6E] hover:text-black dark:text-white   dark:hover:text-[#6E6E6E] md:w-1/3 
+                md:text-3xl`}
+            href={'/'}
+          >
+            Jixin Zheng
+          </Link>
+          <p
+            className={`w-full text-center  text-lg  text-[#6E6E6E] hover:text-black dark:text-white   dark:hover:text-[#6E6E6E] md:w-1/3 
+                md:text-3xl`}
+          >
+            This site is currently under maintenance. Please check back later.
+          </p>
+        </div>
+      ): (
+        <div className="fixed z-10 	flex h-screen w-full flex-col place-content-center items-center gap-y-5 px-4  py-4 align-middle md:inset-y-1/2 md:h-auto md:flex-row md:gap-x-5  md:px-16 md:py-5 lg:px-32">
+          {menuItems &&
+            menuItems.map((menuItem, key) => {
+              const href = resolveHref(menuItem?._type, menuItem?.slug)
+              if (!href) {
+                return null
+              }
+              return (
+                <Link
+                  key={key}
+                  className={`w-full text-center  text-xl text-[#6E6E6E] hover:text-black dark:text-white dark:hover:text-[#6E6E6E]   md:w-1/3 md:text-3xl ${
+                    menuItem?._type === 'home' ? 'font-extrabold ' : ''
+                  }`}
+                  href={href}
+                >
+                  {menuItem.title}
+                </Link>
+              )
+            })}
+        </div>
+      )}
+    </>
   )
 }
